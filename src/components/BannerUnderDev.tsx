7045e26e450fa68396266ee820b5bf649878fa6b
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-09-08 08:14:20 +0700

    fix `BannerUnderDev`

--- src/components/BannerUnderDev.tsx
+++ src/components/BannerUnderDev.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { GrFormClose } from "react-icons/gr";
 import { MdOutlineClose } from "react-icons/md";
 
 const BannerUnderDev = () => {
@@ -9,9 +8,12 @@ const BannerUnderDev = () => {
   return show && (
     <div className="p-2 flex justify-center items-center bg-gradient-to-tl from-green-400 via-teal-400 to-sky-500 text-xs text-center w-full relative">
       <div className="">
-        This site is not completed yet and currently under development.
+        🚧 This site is not completed yet and currently under development. 🚧
       </div>
-      <button className="absolute right-10 text-base bg-transparent text-white ml-auto">
+      <button
+        className="absolute right-10 text-base bg-transparent text-white ml-auto hover:scale-125 transition"
+        onClick={() => setShow(false)}
+      >
         <MdOutlineClose/>
       </button>
     </div>
