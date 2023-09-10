16a375a00022028cec572235a73c9a6202558a03
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-09-11 06:24:07 +0700

    add `portfolios` page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,60 @@
 import MainLayout from "@/components/layouts/MainLayout";
 import UnderConstruction from "@/components/UnderConstruction";
+import { PROJECTS } from "@/dummy-data/projects";
+import moment from "moment/moment";
+import { Badge } from "@/components/shadcn/ui/badge";
+import Link from "next/link";
+import BackToTop from "@/components/BackToTop";
 
 const ProjectsPage = () => {
   return (
     <MainLayout>
-      <UnderConstruction/>
+      <div>
+        <div className="px-4 py-2 border-b-[.5px] border-neutral-700/30">
+          <div className="flex flex-col gap-1">
+            <h1 className="font-semibold text-sm">
+              Maulana Adam Sahid's Projects
+            </h1>
+            <div className="text-neutral-500 font-light text-xs">
+              <p>{PROJECTS.length} Projects</p>
+            </div>
+          </div>
+        </div>
+        
+        {/*Blogs*/}
+        <div>
+          {PROJECTS.map((project) => (
+            <div key={project.id} className="p-4 flex flex-col gap-2 border-b-[.5px] border-neutral-700/30">
+              <div>
+                <Link href={`/projects/${project.slug}`}>
+                  <h1 className="text-xl font-bold hover:underline">
+                    {project.title}
+                  </h1>
+                </Link>
+                
+                <p className="text-neutral-500 text-xs font-light">
+                  Authored by {project.author.name}{' '}
+                  {project.updated_at?.toISOString() === project.created_at?.toISOString()
+                    ? moment(project.created_at).fromNow()
+                    : `(updated ${moment(project.updated_at).fromNow()})`
+                  }
+                </p>
+              </div>
+              
+              <div className="dark flex gap-2">
+                {project.categories.map((c) => (
+                  <Link key={c.id} href={`/projects?category=${c.id}`}>
+                    <Badge variant='secondary' className="rounded-sm font-light">
+                      {c.name}
+                    </Badge>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+        <BackToTop/>
+      </div>
     </MainLayout>
   );
 };
---
+++ src/dummy-data/projects.ts
@@ -0,0 +1,59 @@
+import { Author, AUTHORS } from "@/dummy-data/author";
+import { CATEGORIES, Category } from "@/dummy-data/category";
+import { BODY_1, BODY_2 } from "@/dummy-data/blog-bodies";
+
+export type Project = {
+  id: number;
+  author: Author;
+  slug: string;
+  title: string;
+  body: string;
+  link?: string;
+  gh_link?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  deleted_at?: Date;
+  categories: Category[];
+}
+
+export interface ProjectPreview extends Omit<Project, "body">{}
+
+export const PROJECTS: Project[] = [
+  {
+    id: 1,
+    slug: "rent-payment-records-checker",
+    title: "Rent Payment Records' Checker",
+    body: BODY_1,
+    author: AUTHORS[0],
+    categories: [
+      CATEGORIES[0], CATEGORIES[1],
+    ],
+    created_at: new Date(2022, 8, 34),
+    updated_at: new Date(2023, 8, 11),
+  },
+  {
+    id: 2,
+    slug: "chat-app-with-socket-io",
+    title: "Chat App with Socket.io",
+    body: BODY_2,
+    author: AUTHORS[1],
+    categories: [
+      CATEGORIES[3], CATEGORIES[2],
+    ],
+    created_at: new Date(2022, 8, 11),
+    updated_at: new Date(2023, 8, 11),
+  },
+  {
+    id: 4,
+    slug: "mern-portfolio-page",
+    title: "MERN Portfolio Page",
+    body: BODY_2,
+    author: AUTHORS[1],
+    categories: [
+      CATEGORIES[4],
+    ],
+    created_at: new Date(2022, 8, 10),
+    updated_at: new Date(2023, 8, 11),
+  },
+];
+
